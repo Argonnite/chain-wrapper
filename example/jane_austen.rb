f57981edb274cd5865b9f243c19dc9d3a47134a1
Ron Galicia <dermalin3k@hotmail.com>
2015-07-20 08:11:06 +0000

    Initial check-in

---
+++ example/jane_austen.rb
@@ -0,0 +1,14 @@
+require_relative '../lib/chainWrapper.rb'
+
+
+chainObj = ChainWrapper.new(1)
+chainObj.loadExclusions '../dat/exclusions.dat'
+chainObj.addSource 'http://www.gutenberg.org/cache/epub/1342/pg1342.txt' #pride & prejudice
+chainObj.addSource '../dat/sayings.dat'  #add some non-jane sayings
+#chainObj.dumpChain 'blah.json'
+#chainObj.clearChain
+#chainObj.loadChain 'blah.json'
+for i in 1..10
+  puts chainObj.generate
+end
+
