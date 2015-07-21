2f85820d51ac51f59c5e2131d5fe42a7a2bbcd91
Argonnite <dermalin3k@hotmail.com>
2015-07-21 11:28:47 +0000

    debugging

--- example/jane_austen.rb
+++ example/jane_austen.rb
@@ -1,12 +1,25 @@
 require_relative '../lib/chainWrapper.rb'
 
+#DEBUG=true
+DEBUG=false
 
 chainObj = ChainWrapper.new(3)
 chainObj.loadExclusions '../dat/exclusions.dat'
-#chainObj.addSource 'http://www.gutenberg.org/cache/epub/1342/pg1342.txt' #pride & prejudice
-chainObj.addSource 'http://www.gutenberg.org/cache/epub/31100/pg31100.txt' #austen's complete works!
+chainObj.addSource 'http://www.gutenberg.org/cache/epub/1342/pg1342.txt' #pride & prejudice
+#chainObj.addSource 'http://www.gutenberg.org/cache/epub/31100/pg31100.txt' #austen's complete works!
 
-for i in 1..30
-  puts chainObj.generate
+if DEBUG
+  ### test file integrity
+  chainObj.dumpChain 'jane_chain_1.json'
+  chainObj.clearChain
+  chainObj.loadChain 'jane_chain_1.json'
+  chainObj.dumpChain 'jane_chain_2.json'
+  chainObj.dumpStarters 'jane_starters_1.json'
+  chainObj.clearStarters
+  chainObj.loadStarters 'jane_starters_1.json'
+  chainObj.dumpStarters 'jane_starters_2.json'
+else
+  for i in 1..30
+    puts chainObj.generate
+  end
 end
-
