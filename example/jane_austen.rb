c91d12fb6608a52234a552fbef99f2b3e7ee5eeb
Argonnite <dermalin3k@hotmail.com>
2015-07-21 11:18:14 +0000

    more Austen

--- example/jane_austen.rb
+++ example/jane_austen.rb
@@ -6,7 +6,7 @@ chainObj.loadExclusions '../dat/exclusions.dat'
 #chainObj.addSource 'http://www.gutenberg.org/cache/epub/1342/pg1342.txt' #pride & prejudice
 chainObj.addSource 'http://www.gutenberg.org/cache/epub/31100/pg31100.txt' #austen's complete works!
 
-for i in 1..10
+for i in 1..30
   puts chainObj.generate
 end
 
